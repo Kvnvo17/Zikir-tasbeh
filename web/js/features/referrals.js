1b99c0f84260b0ecb143e341ad9ff2850d38b3c3
Umidjon Quvonov <u7775822@gmail.com>
2026-08-23 16:27:17 +0500

    Refactor fetchInfo and openModal functions
    Updated fetchInfo function to await API call and modified referral link. Enhanced openModal function with improved button and paragraph formatting.

--- web/js/features/referrals.js
+++ web/js/features/referrals.js
@@ -1,16 +1,34 @@
 window.Referrals = (function () {
+
   async function fetchInfo() {
-    return API.get("/referrals/me");
+    const info = await API.get("/referrals/me");
+
+    // your_bot ni ZikrBogiBot ga almashtirish
+    if (info && info.link) {
+      info.link = info.link.replace(
+        "https://t.me/your_bot",
+        "https://t.me/ZikrBogiBot"
+      );
+    }
+
+    return info;
   }
 
   function openModal(info) {
     Modal.open(`
       <p class="section-title">Do'stlarni taklif qilish</p>
+
       <div class="referral-link-box">
         <span id="refLinkText">${info.link}</span>
       </div>
-      <button class="btn block" style="margin-top:12px" id="copyRefBtn">\u{1F4CB} Nusxa olish</button>
-      <p style="font-size:12px;color:#8a90a3;margin-top:12px">Taklif qilingan do'stlar: <b>${info.invited_count}</b></p>
+
+      <button class="btn block" style="margin-top:12px" id="copyRefBtn">
+        📋 Nusxa olish
+      </button>
+
+      <p style="font-size:12px;color:#8a90a3;margin-top:12px">
+        Taklif qilingan do'stlar: <b>${info.invited_count}</b>
+      </p>
     `);
 
     document.getElementById("copyRefBtn").addEventListener("click", () => {
@@ -21,5 +39,9 @@ window.Referrals = (function () {
     });
   }
 
-  return { fetchInfo, openModal };
+  return {
+    fetchInfo,
+    openModal
+  };
+
 })();
