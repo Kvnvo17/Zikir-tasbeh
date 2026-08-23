3ba1dac80e08a0ca9f881450e6e482a06fac1643
Umidjon Quvonov <u7775822@gmail.com>
2026-08-23 16:57:53 +0500

    Refactor fetchInfo function and clean up code

--- web/js/features/referrals.js
+++ web/js/features/referrals.js
@@ -1,46 +1,19 @@
 window.Referrals = (function () {
+async function fetchInfo() {
+return API.get("/referrals/me");
+}
 
-  async function fetchInfo() {
-    const info = await API.get("/referrals/me");
+function openModal(info) {
+Modal.open(  <p class="section-title">Do'stlarni taklif qilish</p>   <div class="referral-link-box">   <span id="refLinkText">${info.link}</span>   </div>   <button class="btn block" style="margin-top:12px" id="copyRefBtn">\u{1F4CB} Nusxa olish</button>   <p style="font-size:12px;color:#8a90a3;margin-top:12px">Taklif qilingan do'stlar: <b>${info.invited_count}</b></p>  );
 
-    // your_bot ni ZikrBogiBot ga almashtirish
-    if (info && info.link) {
-      info.link = info.link.replace(
-        "https://t.me/ZikrBogiBot"
-      );
-    }
+document.getElementById("copyRefBtn").addEventListener("click", () => {  
+  navigator.clipboard.writeText(info.link).then(() => {  
+    Toast.show("Nusxalandi!");  
+    TG.haptic("success");  
+  });  
+});
 
-    return info;
-  }
+}
 
-  function openModal(info) {
-    Modal.open(`
-      <p class="section-title">Do'stlarni taklif qilish</p>
-
-      <div class="referral-link-box">
-        <span id="refLinkText">${info.link}</span>
-      </div>
-
-      <button class="btn block" style="margin-top:12px" id="copyRefBtn">
-        📋 Nusxa olish
-      </button>
-
-      <p style="font-size:12px;color:#8a90a3;margin-top:12px">
-        Taklif qilingan do'stlar: <b>${info.invited_count}</b>
-      </p>
-    `);
-
-    document.getElementById("copyRefBtn").addEventListener("click", () => {
-      navigator.clipboard.writeText(info.link).then(() => {
-        Toast.show("Nusxalandi!");
-        TG.haptic("success");
-      });
-    });
-  }
-
-  return {
-    fetchInfo,
-    openModal
-  };
-
+return { fetchInfo, openModal };
 })();
